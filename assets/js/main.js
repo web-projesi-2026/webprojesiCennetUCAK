6ee8a76b707ad031767cd22f399159a93cc3172b
Cennet uçak <ucakcennet83@gmail.com>
2026-04-12 17:30:27 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+if (menuToggle && navMenu) {
+  menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("show");
+  });
+}
